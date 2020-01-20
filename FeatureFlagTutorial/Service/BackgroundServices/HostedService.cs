03f923f4f59273db3d236ef4a7bcf9d820a37ddd
GW\vtram <vtram@glasswallsolutions.com>
2020-01-20 09:47:01 +0000

    Illustrate ability to check for feature flag using if...else blocks.

--- FeatureFlagTutorial/Service/BackgroundServices/HostedService.cs
+++ FeatureFlagTutorial/Service/BackgroundServices/HostedService.cs
@@ -3,16 +3,19 @@ using System.Threading;
 using System.Threading.Tasks;
 using Glasswall.Kernel.Logging;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
 
 namespace Glasswall.FileTrust.RepoName.Service.BackgroundServices
 {
     public class HostedService : BackgroundService
     {
         private readonly IGWLogger<HostedService> _logger;
+        private readonly IFeatureManager _featureManager;
 
-        public HostedService(IGWLogger<HostedService> logger)
+        public HostedService(IGWLogger<HostedService> logger, IFeatureManager featureManager)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _featureManager = featureManager;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,6 +25,20 @@ namespace Glasswall.FileTrust.RepoName.Service.BackgroundServices
             // Background logic goes here.
 
             _logger.Log(LogLevel.Information, (int)Common.Logging.EventId.ServiceStarted, "The hosted service has started.");
+
+            while (true)
+            {
+                if (await _featureManager.IsEnabledAsync(nameof(MyFeatureFlags.Beta)))
+                {
+                    _logger.Log(LogLevel.Information, (int)Common.Logging.EventId.ServiceStarted, "My feature is Enabled!");
+                }
+                else
+                {
+                    _logger.Log(LogLevel.Information, (int)Common.Logging.EventId.ServiceStarted, "My feature is Disabled!");
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
 
             await Task.Delay(Timeout.Infinite, stoppingToken);
         }
